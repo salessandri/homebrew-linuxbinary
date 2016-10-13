db64a5bb8aac10f66edf39e4c9ab707cb64162a2
okampfer <okampfer@gmail.com>
2016-10-13 22:31:59 +0800

    Add dbeaver community version formula.

---
+++ Formula/dbeaver-ce.rb
@@ -0,0 +1,19 @@
+class DbeaverCe < Formula
+    desc "Dbeaver Community version. Free multi-platform database tool for developers, SQL programmers, database administrators and analysts."
+    homepage "http://dbeaver.jkiss.org/"
+    version :latest
+    sha256 :no_check
+
+    if MacOS.prefer_64_bit?
+        url "http://dbeaver.jkiss.org/files/dbeaver-ce-latest-linux.gtk.x86_64.tar.gz"
+    else
+        url "http://dbeaver.jkiss.org/files/dbeaver-ce-latest-linux.gtk.x86.tar.gz"
+    end
+
+    bottle :unneeded
+
+    def install
+        libexec.install Dir["*"]
+        bin.install_symlink("#{libexec}/dbeaver")
+    end
+end
